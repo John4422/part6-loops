208295bcce166d97efc01f6d75489710704f8ed8
John4422 <Johnstein456@hotmail.com>
2022-10-19 11:30:06 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿namespace part6_loops
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 5; i <= 50; i+=5) //i++ is the same as i = i + 1 and i += 1
+            {
+                Console.Write(i);
+            }
+        }
+    }
+}
